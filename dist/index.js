f93a7e1f91c95a218ddf33b03cbfe137760b55ef
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-20 15:47:42 +0600

    create dst file

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const student = { name: "Sraboni", id: 223311064, section: "B" };
+console.log(student);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const student: { name: string, id: number, section: string }
+    = { name: "Sraboni", id: 223311064, section: "B" }
+
+console.log(student)
