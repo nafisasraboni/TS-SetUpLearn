9b435205899e0d729faa1671c1321ef7ac0f5fe1
nafisasraboni <nafisasraboni2207@gmail.com>
2026-08-20 16:29:11 +0600

    learn type alias/custom type in ts

---
+++ src/type/product.ts
@@ -0,0 +1,24 @@
+type Product={
+    id:number;
+    name:string;
+    price:number;
+    description?:string
+}
+
+const products : Product[] = [{
+    id:1,
+    name:"Laptop",
+    price:999.99,
+    description:"High Performence Laptop"
+}]
+const products1 : Product[] = [{
+    id:2,
+    name:"EarBuds",
+    price:350,
+    description:"High Performence earbuds"
+}]
+const products2 : Product[] = [{
+    id:3,
+    name:"Power Bank",
+    price:675
+}]
---
+++ src/type/type.ts
@@ -0,0 +1,12 @@
+type Gender = "Male"|"Female"|"Others";
+
+type Student = { name: string; id: number; section: string }
+
+const student: Student = { name: "Sraboni", id: 223311064, section: "B" }
+
+console.log(student)
+
+const student1: Student = { name: "Payel", id: 223311065, section: "c" }
+const student2: Student = { name: "R.k", id: 223311066, section: "a" }
+const student3: Student = { name: "Shimul", id: 223311067, section: "b" }
+const student4: Student = { name: "Moumita", id: 223311068, section: "c" }
